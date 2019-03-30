97d0ce7c73489cbb2fb8d52bb73af4c8738f0ff0
Clive <874760401@qq.com>
2019-03-30 17:12:34 +0800

    'add'

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 define('TOKEN','clive31');
 
-class WexinController extends Controller
+class WeixinController extends Controller
 {
     public function index()
     {
