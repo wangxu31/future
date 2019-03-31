1314e687d1f2e6b561793b6dd0cc4bc5b464a50d
wangxu31 <874760401@qq.com>
2019-03-31 22:24:51 +0800

    add

---
+++ app/Enums/RecordType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+class RecordType
+{
+	const ONLINE_SALE = 1;
+	const THIRD_PARTY_SALE = 2;
+
+	const ORDER_TYPE_DICT = [
+		self::ONLINE_SALE => '线上商城',
+		self::THIRD_PARTY_SALE => '外部',
+	];
+}
